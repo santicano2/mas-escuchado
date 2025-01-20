ea96dc2c772eb5e7c288af85e085cd5a64418f8d
Santi <canosantiago404@gmail.com>
2025-01-20 11:32:07 -0300

    feat: data mock artists

---
+++ app/data/artists.ts
@@ -0,0 +1,56 @@
+export interface Artist {
+  name: string;
+  monthlyListeners: number;
+  imageUrl: string;
+}
+
+export const artists: Artist[] = [
+  {
+    name: "Bad Bunny",
+    monthlyListeners: 67000000,
+    imageUrl:
+      "https://images.unsplash.com/photo-1618519764620-7403abdbdfe9?auto=format&fit=crop&q=80&w=1200",
+  },
+  {
+    name: "Peso Pluma",
+    monthlyListeners: 45000000,
+    imageUrl:
+      "https://images.unsplash.com/photo-1493225457124-a3eb161ffa5f?auto=format&fit=crop&q=80&w=1200",
+  },
+  {
+    name: "Karol G",
+    monthlyListeners: 52000000,
+    imageUrl:
+      "https://images.unsplash.com/photo-1501386761578-eac5c94b800a?auto=format&fit=crop&q=80&w=1200",
+  },
+  {
+    name: "Feid",
+    monthlyListeners: 37000000,
+    imageUrl:
+      "https://images.unsplash.com/photo-1470225620780-dba8ba36b745?auto=format&fit=crop&q=80&w=1200",
+  },
+  {
+    name: "Rauw Alejandro",
+    monthlyListeners: 42000000,
+    imageUrl:
+      "https://images.unsplash.com/photo-1501281668745-f7f57925c3b4?auto=format&fit=crop&q=80&w=1200",
+  },
+  {
+    name: "Bizarrap",
+    monthlyListeners: 39000000,
+    imageUrl:
+      "https://images.unsplash.com/photo-1514525253161-7a46d19cd819?auto=format&fit=crop&q=80&w=1200",
+  },
+  {
+    name: "Myke Towers",
+    monthlyListeners: 35000000,
+    imageUrl:
+      "https://images.unsplash.com/photo-1493225457124-a3eb161ffa5f?auto=format&fit=crop&q=80&w=1200",
+  },
+  {
+    name: "Shakira",
+    monthlyListeners: 55000000,
+    imageUrl:
+      "https://images.unsplash.com/photo-1516450360452-9312f5e86fc7?auto=format&fit=crop&q=80&w=1200",
+  },
+];
